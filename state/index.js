ace6ffffe4ec1ab55afeb577315d1809d2db80a0
t32lin <toronto.taolin@gmail.com>
2024-01-12 08:55:15 -0500

    commit-0112-2

--- state/index.js
+++ state/index.js
@@ -1,6 +1,6 @@
 import { createGlobalState } from "react-hooks-global-state";
 const { setGlobalState, useGlobalState } = createGlobalState({
-    Sector: '',
+    Sector: 'Semiconductor',
     Company: '',
     Tab: 0
 });
