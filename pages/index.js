c5c508bc41fa56909b4816d8dcdd927b7cc81079
t32lin <toronto.taolin@gmail.com>
2024-01-12 08:11:28 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -7,6 +8,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className='container grid grid-rows-subgrid gap-4 row-span-2'>
+
+        <h2 className='my-10 text-3xl font-medium'>Methodology</h2>
+
+      </div >
     </main>
   )
 }
